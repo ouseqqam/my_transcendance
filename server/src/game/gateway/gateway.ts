b05a5745853f34579d8b5a087e48cfcdf33f12ad
Oussama Seqqam <ouseqqam@student.1337.ma>
2022-11-30 19:07:36 +0100

    learn things of socket

--- server/src/game/gateway/gateway.ts
+++ server/src/game/gateway/gateway.ts
@@ -11,28 +11,38 @@ import { Server, Socket } from 'socket.io'
 export class Mygeteway implements OnModuleInit {
     @WebSocketServer()
     server: Server
-    sockets = new Map<string, Socket>()
-    request = new Map<string, Socket>()
+    count  = 2
+    roomData = new Map<string, string[]>([])
+    roomName = ''
  
     onModuleInit() {
         this.server.on('connection', socket => {
-            console.log('connected')
-             console.log(socket.id)
-            this.sockets.set(socket.id, socket)
+            // console.log('connected')
+            // console.log(socket.id)
         })
     }
 
-    @SubscribeMessage('join')
-    createRoom(@MessageBody() data: any,@ConnectedSocket()  socket: Socket) {
-        socket.join(data.room)
+    @SubscribeMessage('joinToRoom')
+    JoinToRoom(roomName: string,@ConnectedSocket()  socket: Socket) {
+        socket.join(roomName)
     }
 
     @SubscribeMessage('start')
-    startGame(@MessageBody() data: any,@ConnectedSocket()  socket: Socket) {
-        socket.to(data.room).emit('start')
+    startGame(@MessageBody() data: any, @ConnectedSocket()  socket: Socket) {
+        if (this.count == 2) {
+            this.roomName = Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36)
+            this.count = 0
+        }
+        this.count++
+        socket.join(this.roomName)
+        this.server.to(this.roomName).emit('onMessage', "hello1")
+        let socketArray = this.roomData.get(this.roomName)
+        if (socketArray) {
+            this.roomData.set(this.roomName, [...socketArray, socket.id])
+        } else {
+            this.roomData.set(this.roomName, [socket.id])
+        }
+        console.log(this.roomData)
     }
-
-    @SubscribeMessage('requestToJoin')
-    requestToJoin(@MessageBody() data: any,@ConnectedSocket()  socket: Socket) {
 
 }
