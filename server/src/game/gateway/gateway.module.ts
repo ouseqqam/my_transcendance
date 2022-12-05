1cff958360e1b40e7fa89e4e863fa7a59be39731
Oussama Seqqam <ouseqqam@student.1337.ma>
2022-12-05 19:25:02 +0100

    change some event socket

--- server/src/game/gateway/gateway.module.ts
+++ server/src/game/gateway/gateway.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { Mygeteway } from './gateway';
-import { SocketClients } from './socketClients';
-
 
 @Module({
     imports: [Mygeteway],
-    providers: [SocketClients],
 })
 export class GatwayModule {}
--- server/src/game/gateway/gateway.ts
+++ server/src/game/gateway/gateway.ts
@@ -1,6 +1,7 @@
 import { OnModuleInit } from "@nestjs/common";
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server, Socket } from 'socket.io'
+import { ball, player1, player2, stage } from './data'
 
 @WebSocketGateway({
     cors: {
@@ -11,9 +12,14 @@ import { Server, Socket } from 'socket.io'
 export class Mygeteway implements OnModuleInit {
     @WebSocketServer()
     server: Server
+
     count  = 2
-    roomData = new Map<string, string[]>([])
+    roomData = new Map<string, any>([])
     roomName = ''
+    beStage = stage
+    beBall = ball
+    bePlayer1 = player1
+    bePlayer2 = player2
  
     onModuleInit() {
         this.server.on('connection', socket => {
@@ -23,26 +29,80 @@ export class Mygeteway implements OnModuleInit {
     }
 
     @SubscribeMessage('joinToRoom')
-    JoinToRoom(roomName: string,@ConnectedSocket()  socket: Socket) {
+    JoinToRoom(roomName: string, @ConnectedSocket() socket: Socket) {
         socket.join(roomName)
+        let socketArray = this.roomData.get(this.roomName)
+        this.roomData.set(this.roomName, [...socketArray,{
+            "socketId": socket.id,
+            "role": "watcher"
+        }])
+        this.server.to(roomName).emit("onMessage", "you can now watch the game")
     }
 
-    @SubscribeMessage('start')
-    startGame(@MessageBody() data: any, @ConnectedSocket()  socket: Socket) {
+    @SubscribeMessage('findGame')
+    reqToJoin(@ConnectedSocket()  socket: Socket) {
+        console.log(socket.id)
+        let exist = 0
         if (this.count == 2) {
             this.roomName = Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36)
             this.count = 0
         }
-        this.count++
         socket.join(this.roomName)
-        this.server.to(this.roomName).emit('onMessage', "hello1")
         let socketArray = this.roomData.get(this.roomName)
-        if (socketArray) {
-            this.roomData.set(this.roomName, [...socketArray, socket.id])
-        } else {
-            this.roomData.set(this.roomName, [socket.id])
+        this.roomData.forEach((value) => {
+            if (value.includes(socket.id)) {
+                socket.emit("onMessage", "You are already in the room")
+                exist = 1
+                return
+            }
+        })
+        if (exist == 0) {
+            if (socketArray) {
+                this.roomData.set(this.roomName, [...socketArray,{
+                    "socketId": socket.id,
+                    "role": this.count == 0 ? "player1" : "player2",
+                    "score": 0,
+                }])
+            } else {
+                this.roomData.set(this.roomName, [{
+                    "socketId": socket.id, 
+                    "role": this.count == 0 ? "player1" : "player2",
+                    "score": 0,
+                }])
+            }
+            this.count++
+            if (this.count == 1) {
+                socket.emit("onMessage", "Waiting for another player to join")
+            }
+            else if (this.count == 2) {
+                this.server.to(this.roomName).emit("joinRoom", {
+                    status: "Pending",
+                    roomName: this.roomName,
+                    player1: this.roomData.get(this.roomName)[0]["socketId"],
+                    player2: this.roomData.get(this.roomName)[1]["socketId"],
+                })
+            }
         }
         console.log(this.roomData)
     }
 
+    @SubscribeMessage('startGame')
+    startGame(@MessageBody() data: any, roomName: string) {
+        this.server.to(roomName).emit("onMessage", "game started")
+        const maxW = this.beStage.w - 1.5 / 2 - this.beBall.args[0] / 2
+        const maxH = this.beStage.h - 1.5 / 2 - this.beBall.args[0] / 2
+        let signalX = Math.random() > 0.5 ? 1 : -1
+        let signalY = Math.random() > 0.5 ? 1 : -1
+
+        setInterval(() => {
+            if (this.beBall.position.x >= maxW || this.beBall.position.x <= -maxW)
+                signalX *= -1
+            this.beBall.position.x += signalX
+            if (this.beBall.position.y >= maxH || this.beBall.position.y <= -maxH)
+                signalY *= -1
+            this.beBall.position.y += signalY
+            this.server.to(roomName).emit("onMessage", "hello")
+            console.log(this.beBall.position)
+        }, 1000 /110)
+    }
 }
